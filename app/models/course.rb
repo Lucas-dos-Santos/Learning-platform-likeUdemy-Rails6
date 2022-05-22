25378ba27bfad3eaa11b881f8395da9952aa3a84
Lucas dos Santos <lucas.ffr2014@gmail.com>
2022-05-22 10:01:23 -0300

    Add relationship course belongs to user, and user has many courses

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,4 +2,6 @@ class Course < ApplicationRecord
 	validates :title, presence: true
 	validates :description, presence: true, length: { minimum: 5 }
 	has_rich_text :description
+
+	belongs_to :user
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,9 @@
+User.create!(email: 'lucas.ffr2014@gmail.com', password: 'bravo5')
+
 30.times do
   Course.create!([{
     title: Faker::Educator.course_name,
-    description: Faker::TvShows::GameOfThrones.quote
+    description: Faker::TvShows::GameOfThrones.quote,
+    user_id: User.first.id
   }])
 end
