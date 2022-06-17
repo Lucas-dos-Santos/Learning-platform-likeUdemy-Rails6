b9d4300ace74ce2ca339294897ce0d9d27b668f8
Lucas dos Santos <lucas.ffr2014@gmail.com>
2022-06-17 11:21:26 -0300

    Remove action text, and install CKeditor

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,7 +1,6 @@
 class Course < ApplicationRecord
 	validates :title, presence: true
 	validates :description, presence: true, length: { minimum: 5 }
-	has_rich_text :description
 
 	extend FriendlyId
   friendly_id :title, use: :slugged
